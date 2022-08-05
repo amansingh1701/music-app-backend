097e5a8a20c2ccf11e1dec06ce8cf76fda1e045b
Aman Kumar Singh <amankumar.singh@softwaysolutions.com>
2022-08-05 21:57:56 +0530

    initial trial 3

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5001;
 
+server.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 server.use(bodyParser.json({ limit: "10mb" }));
 server.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
@@ -112,12 +135,12 @@ server.get("/getImage", (req, res) => {
   });
 });
 
-const corsOptions = {
-  origin: [
-    "http://127.0.0.1:5501",
-    "https://stellar-hotteok-ee8d14.netlify.app",
-  ],
-  methods: ["GET", "PUT", "POST"],
-};
+// const corsOptions = {
+//   origin: [
+//     "http://127.0.0.1:5501",
+//     "https://stellar-hotteok-ee8d14.netlify.app",
+//   ],
+//   methods: ["GET", "PUT", "POST"],
+// };
 
-server.use(cors(corsOptions));
+// server.use(cors(corsOptions));
